afd74b9580ed8a98e65aa68a4a04318447cb7e86
Julian Correa <julian@torre.co>
2018-11-14 07:15:04 -0500

    btn danger

---
+++ src/components/buttonDanger/index.ts
@@ -0,0 +1,14 @@
+import { html, LitElement } from '@polymer/lit-element';
+
+const css = require('./index.scss') as string;
+
+class BtnPrimary extends LitElement {
+  public render() {
+    return html`
+      <style>${css}</style>
+      <button class="btn btn-primary"><slot></slot></button>
+    `;
+  }
+}
+
+window.customElements.define('btn-danger', BtnPrimary);
