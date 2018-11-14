c972378acaf5a7ddbab72acb876796c04d50c989
Julian Correa <julian@torre.co>
2018-11-14 07:16:06 -0500

    removed unnecessary comments

--- src/components/buttonPrimary/index.ts
+++ src/components/buttonPrimary/index.ts
@@ -3,8 +3,6 @@ import { html, LitElement } from '@polymer/lit-element';
 const css = require('./index.scss') as string;
 
 class BtnPrimary extends LitElement {
-
-  // Render method should return a `TemplateResult` using the provided lit-html `html` tag function
   public render() {
     return html`
       <style>${css}</style>
